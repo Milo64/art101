99cd7c1100b6491f4ac0b031926c80e259a15948
Milo <yannelirodriguez0810@gmail.com>
2024-10-17 14:17:51 -0700

    Lab4 changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,13 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Rock-Paper-Scissors: 
+//Two players gather 
+//Each player extends one hand out
+//Each player makes a fist with their hand
+//Each player shakes their hand 3 times while saying rock-paper-scissors
+//After saying if 3 time they show both of their hands 
+//Outcome 1: Player1 shows Paper and Player 2  shows Rock
+//Player1 covers Rock and wins.
+//Outcome 2: Player 2 shows Scissors  and Player 1 shows Paper
+//Player2 cuts paper and wins
+//Outcome 3: Player1 show Rock and Player 2 shows  Scissors
+//Player1 Rock crushes scissors and wins
+//Outcome 4: Both player tie.
