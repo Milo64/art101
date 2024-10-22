1ce7b2e8c650df16230f7d507470537a617afa45
Milo <yannelirodriguez0810@gmail.com>
2024-10-21 19:32:22 -0700

    Lab 5 changes

---
+++ lab5/js/lab5.js
@@ -0,0 +1,20 @@
+
+// Author: Milo Rodriguez 
+// Date: October 21, 2024
+// License: MILO_LAND
+
+// Define Variable
+make= "Hyundai";
+model= "CRV";
+color= "Green";
+year= "2022";
+
+//calculate 
+age= 2024 - year;
+
+//output
+document.writeln("Hyundai:" + Hyundai+ "<br>" );
+document.writeln("CRV:" + CRV + "<br>");
+document.writeln("Green:"+ Green + "<br>");
+document.writeln("2022:" + 2022 + "<br>");
+document.writeln("2024:" + 2024 + "2024<br>");
