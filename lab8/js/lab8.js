873c87854b93df21c3704df8974b4c5df095f60a
Milo <yannelirodriguez0810@gmail.com>
2024-10-31 16:16:48 -0700

    lab8 chnages

---
+++ lab8/js/lab8.js
@@ -0,0 +1,21 @@
+
+// Author: Milo Rodriguez 
+// Date: October 29, 2024
+// License: MILO_LAND
+
+function Adding(x){  
+   var results = 20 + x;
+   return results;}
+//test function
+   console.log("What is 20 plus x?", addding(0));
+   console.log("what is 20 plus x?", addiing(2));
+
+   myAarray = [1,2,3,4,5,6];
+   console.log("My Aarray", myAarray);
+
+   var results= myAarray.map(Adding);
+   console.log("results:", results);
+
+   myArray.map(function(x){                                  
+      var results = 15 - x;                                     
+       return results;})
