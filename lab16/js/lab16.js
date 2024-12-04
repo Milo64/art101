df51ae267fd9bfc16bcb26ccc5c8e56bf7528c1a
Milo <yannelirodriguez0810@gmail.com>
2024-12-03 20:44:19 -0800

    Lab16

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -1,3 +1,29 @@
 // Author: Milo Rodriguez 
-// Date: Nov 26th, 2024
+// Date: Dec 3rd, 2024
 // License: MILO_LAND
+
+$(comic).ready(function (){
+
+const comicUrl  = "https://xkcd.com/614/info.0.json";
+
+$a.jax({
+
+    URL: comicUrl,
+    typeof: "GET",
+    dataType: "json",
+    success: function (data) {
+
+        const comic = data;
+
+        $('#output').append('<h3>${comic.title}</h3');
+        $('#output').append(
+        <img src="${comic.img}"></img>
+
+        );
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+        consolelog("ERROR:", textStatus, errorThrown);
+    }
+});
+
+});
